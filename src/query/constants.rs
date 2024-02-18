07aa62157ee8b679489d129ec94a63e0c3c992d1
DW <git@c00.slamil.me>
2024-02-17 21:22:15 -0800

    fix query

--- src/query/constants.rs
+++ src/query/constants.rs
@@ -9,8 +9,8 @@ pub struct PreparedQueries {
 pub fn get_prepared_queries(mode: QueryOpMode) -> PreparedQueries {
 	match mode {
 		QueryOpMode::Misskey => PreparedQueries {
-			get_user: r#"SELECT "followersCount", "followingCount", "notesCount" FROM user WHERE uri = $1"#,
-			get_instance_stats: r#"SELECT "followersCount", "followingCount", "notesCount" FROM instance WHERE host = $1"#,
+			get_user: r#"SELECT t."followersCount", t."followingCount", t."notesCount" FROM public."user" t WHERE uri = $1 LIMIT 1"#,
+			get_instance_stats: r#"SELECT "followersCount", "followingCount", "notesCount" FROM instance WHERE host = $1 LIMIT 1"#,
 		},
 		_ => unimplemented!(),
 	}
